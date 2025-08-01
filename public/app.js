f8c40c74b3f7f1f3db80de57d8c5b35fe83d9b1a
Jota-89 <jota.aca@gmail.com>
2025-08-01 00:18:28 -0600

    Fix Functions connection syntax

--- public/app.js
+++ public/app.js
@@ -17,11 +17,13 @@ const auth = getAuth(app);
 const functions = getFunctions(app);
 
 // Conectar a emuladores locales
-if (location.hostname === '127.0.0.1' || location.hostname === 'localhost' || location.hostname.includes('onrender.com'))
+// Conectar a emuladores locales
+if (location.hostname === '127.0.0.1' || location.hostname === 'localhost' || location.hostname.includes('onrender.com')) {
   if (location.hostname.includes('onrender.com')) {
-  connectFunctionsEmulator(functions, 'localhost', 5001);
-} else {
-  connectFunctionsEmulator(functions, location.hostname, 5001);
+    connectFunctionsEmulator(functions, 'localhost', 5001);
+  } else {
+    connectFunctionsEmulator(functions, location.hostname, 5001);
+  }
 }
 
 // Elementos DOM
